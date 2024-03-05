ea4a58c3922b8205bd60973352ec50857af97dc6
joaopedro <joaoribeiroc3po@gmail.com>
2024-03-05 00:11:16 -0300

    updates

--- RLE_compression.py
+++ RLE_compression.py
@@ -1,39 +1,27 @@
+
 import pydicom
 from pydicom.uid import RLELossless
 import os
 
 dcm_folder_path = 'dicom_files'
+cp_dcm_folder = 'new_compressed_files'
 
 ds_list = []
 
+# compress and save all DICOM files in the folder
 for file in os.listdir(dcm_folder_path):
-    dcm_file_path = os.path.join(dcm_folder_path,file)
+    dcm_file_path = os.path.join(dcm_folder_path, file)
     if os.path.isfile(dcm_file_path) and file.lower().endswith('.dcm'):
         ds = pydicom.dcmread(dcm_file_path)
-        
-        ds_list.append(ds)
-        #tamanho
         size = os.path.getsize(dcm_file_path)
-        print(f'tamanho do arquivo:{ds.PatientName}: {size} bytes')
-        
-        # comprimir 
+        print(f'Tamanho do arquivo antes da compressão - {ds.PatientName}: {size} bytes')
         ds.compress(RLELossless)
-        # muda o nome pra salvar em uma pasta de comprimidos
-        ds.save_as(f'new_compressed_files/cp{file}')
-        
-        
-cp_dcm_folder ='new_compressed_files'
+        new_file_path = os.path.join(cp_dcm_folder, f'cp_{file}')
+        ds.save_as(new_file_path)
+        ds_list.append(new_file_path)
 
-# for file in os.listdir(cp_dcm_folder):
-#     dcm_file_path = os.path.join(dcm_folder_path,file)
-#     if os.path.isfile(dcm_file_path) and file.lower().endswith('.dcm'):
-#         ds = pydicom.dcmread(dcm_file_path)
-        
-#         size = os.path.getsize(dcm_file_path)
-#         print(f'tamanho do arquivo comprimido :{ds.PatientName}: {size} bytes')
-        
+for file_path in ds_list:
+    size2 = os.path.getsize(file_path)
+    file_name = os.path.basename(file_path)
+    print(f' \n Tamanho do arquivo comprimido - {file_name}: {size2} bytes')
     
-    
-    
-    
-
